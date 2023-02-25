b9b8fbf857ee094c7cf7e8418a203702a634e1df
Pirren <43518301+pirrencode@users.noreply.github.com>
2023-02-25 20:49:46 +0200

    Adding statement to close snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.header("The fruit load contains:")
 
 # streamlit.dataframe(my_data_rows)
@@ -81,5 +82,6 @@ if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
+    my_cnx.close()
 
 # streamlit.write('Thanks for adding ', add_my_fruit)
