507e17aac42fa8e3e7b1753fe23cea014212c0af
Pirren <43518301+pirrencode@users.noreply.github.com>
2023-02-25 15:02:20 +0200

    lesson 3 changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
-streamlit.header('Breakfest menu')
-streamlit.text('Omega 3')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
