ea33218c9c3c005c0d4595753f20f4ba85e62135
Pirren <43518301+pirrencode@users.noreply.github.com>
2023-03-20 10:09:05 +0200

    Added new header for bootcamp students

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,11 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🐔 Breakfest should be fun! 🥣')
+streamlit.header('🐔 Monday breakfest should be fun! 🥣')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.text('🥗 Let us have some fun! ')
 
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
