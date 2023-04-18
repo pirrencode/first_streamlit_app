87c4e63d2b5fb718aee295a8458e34df5c7bc5eb
Pirren <43518301+pirrencode@users.noreply.github.com>
2023-04-18 13:45:12 +0300

    removed unused title

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header('🍌🥭 Healhy Lifestyle 🥝🍇')
 
-streamlit.header('🍌🥭 Hello everyone!! 🥝🍇')
-
 #my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
